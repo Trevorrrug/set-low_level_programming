9fe0409b5c441e2525394662d4937d456983fd97
Trevor Rugambage <trevorrugambage@gmail.com>
2026-07-27 13:46:31 -0700

    Complete 0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string to capitalize
+ *
+ * Return: pointer to the string
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+	int j;
+	char sep[] = " \t\n,;.!?\"(){}";
+
+	while (str[i] != '\0')
+	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (str[i] == sep[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+					str[i + 1] -= 32;
+			}
+		}
+		i++;
+	}
+
+	return (str);
+}
